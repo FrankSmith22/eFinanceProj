bab7e8c192604a670e581c45a4db98c1fd926039
FrankSmith22 <fasvi6@gmail.com>
2021-11-05 11:35:07 -0400

    Init commit; Basic login and registration functionality

---
+++ src/main/java/com/example/efinance/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.efinance.controller;
+
+import com.example.efinance.model.User;
+import com.example.efinance.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model)
+    {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+        return "register_success";
+    }
+    @GetMapping("/account_view")
+    public String listUsers(Model model) {
+
+        return "account_view";
+    }
+
+}
---
+++ src/main/java/com/example/efinance/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.efinance.service;
+
+import com.example.efinance.Auth.CustomUserDetails;
+import com.example.efinance.model.User;
+import com.example.efinance.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
