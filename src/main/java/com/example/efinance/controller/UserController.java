0c0a7c1a991c135ae5b26ce5c6bdfaa6b4e76009
FrankSmith22 <fasvi6@gmail.com>
2021-11-06 16:34:06 -0400

    Made some notes for ideas on how to accomplish form generation for proper loan type

--- src/main/java/com/example/efinance/controller/UserController.java
+++ src/main/java/com/example/efinance/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.efinance.controller;
 
-import com.example.efinance.model.User;
+import com.example.efinance.model.*;
 import com.example.efinance.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,8 +53,14 @@ public class UserController {
     }
 
     @PostMapping("/loanApplication")
-    public String showFields(Model model){
+    public String showFields(Model model, Loan loan, AutoLoan autoLoan, BusinessLoan businessLoan, StudentLoan studentLoan){
         System.out.println("post method called");
+
+        // Probably best to use a switch case to figure out which html page should be shown
+        // the currently selected option in the dropdown is probably in the model parameter (using getAttribute)
+        // Will need to modify loan_select.html then to add the selected value to the model upon submission
+
+
         return "loan_select";
     }
 
